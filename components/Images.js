aec503ba5a43b8dc69c6f48d72f74cd6022352d8
Apekshahande <apeksha18@navgurukul.org>
2024-01-02 13:44:22 +1100

    added images

--- components/Images.js
+++ components/Images.js
@@ -4,10 +4,10 @@ import Image from 'next/image';
 
 function Images() {
   const imageUrls = [
-    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
-    'https://images.pexels.com/photos/220429/pexels-photo-220429.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-4.0.3',
-    'https://us.123rf.com/450wm/last19/last191804/last19180400184/100595887-photographer-asian-woman-travel-nature-travel-relax-nature-study-in-the-jungle-thailand.jpg?ver=6',
+    '/images/learning1.jpg',
+    '/images/learning2.jpg',
+    '/images/learning3.jpeg',
+    '/images/learning4.jpg',
   ];
 
   return (
