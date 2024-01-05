9ef8ff9d753a0b8728fc6be84a0bc50f4c36c2a5
Apekshahande <apeksha18@navgurukul.org>
2024-01-05 18:05:27 +1100

    added slider to client page

--- components/Client.js
+++ components/Client.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Grid, Container,Divider } from "@mui/material";
+import { Typography, Grid, Container, Divider } from "@mui/material";
 import Slider from "react-slick";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { breakpoints } from "../styles/constant";
@@ -17,103 +17,65 @@ const Client = () => {
     "/images/ng.svg",
   ];
 
-  if (isActive) {
-    // If it's a mobile view, render an auto-slider with centering
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000, // Set the autoplay speed in milliseconds
-      centerMode: true,
-      centerPadding: "10%", // Adjust the padding as needed
-    };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: isActive ? 1 : 2, // Show one image at a time for smaller screens
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000, // Set the autoplay speed in milliseconds
+    responsive: [
+      {
+        breakpoint: breakpoints.values.sm,
+        settings: {
+          slidesToShow: 1, // Show one image at a time for smaller screens
+        },
+      },
+    ],
+  };
 
-    return (
-      <div
-        style={{
-          width: "100%",
-          backgroundColor: "#F7F7F7",
-          padding: "0px 10px",
-          textAlign: "center",
-          marginTop: "48px",
-        }}
-      >
-        <Typography variant="h4" pt={2} mb={"32px"}>
-          Our Clients
-          <Divider
-          variant="middle"
-          sx={{
-            background: "#4A9088",
-            height: "3px",
-            width: "25%",
-            margin: "auto",
-          }}
-        />
-        </Typography>
-        <Container maxWidth="xl">
-          <Slider {...settings}>
-            {clientLogos.map((logo, index) => (
-              <div key={index} style={{ margin: "0", padding: "0" }}>
-                <img
-                  src={logo}
-                  alt={`Client Logo ${index + 1}`}
-                
-                  style={{
-                    width: index == 0 ? "45%" : "75%",
-                  }}
-                />
-              </div>
-            ))}
-          </Slider>
-        </Container>
-      </div>
-    );
-  }
-
-  // If it's not a mobile view, render the grid
   return (
-    <>
-      <div
-        style={{
-          width: "100%",
-          backgroundColor: "#F7F7F7",
-          padding: "80px 300px",
-          textAlign: "center",
-          marginTop: "48px",
-        }}
-      >
-        <Typography variant="h4" pt={2} mb={"32px"}>
-          Our Clients
-          <Divider
+    <div
+      style={{
+        width: "100%",
+        backgroundColor: "#F7F7F7",
+        padding: isActive ? "0px 10px" : "80px 300px",
+        textAlign: "center",
+        marginTop: "48px",
+      }}
+    >
+      <Typography variant="h4" pt={2} mb={"32px"}>
+        Our Clients
+        <Divider
           variant="middle"
           sx={{
             background: "#4A9088",
             height: "3px",
-            width: "9%",
+            width: isActive ? "25%" : "9%",
             margin: "auto",
           }}
         />
-        </Typography>
-        <Container maxWidth="xl">
-          <Grid container spacing={0}>
-            {clientLogos.map((logo, index) => (
-              <Grid key={index} item xs={4} sm={3} md={3}>
-                <img
-                  src={logo}
-                  alt={`Client Logo ${index + 1}`}
-                  style={{
-                    width: index == 0 ? "45%" : "65%",
-                  }}
-                />
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </div>
-    </>
+      </Typography>
+      <Container maxWidth="md" mt={!isActive&&"48px"}>
+        <Slider {...settings}>
+          {clientLogos.map((logo, index) => (
+            <div key={index} style={{ margin: "0", padding: "0" }}>
+              <img
+                src={logo}
+                alt={`Client Logo ${index + 1}`}
+                style={{
+                  width: "auto",
+                  height: "48px", // Set your fixed height here
+                  objectFit: "cover", // Maintain aspect ratio
+                  margin: "auto"
+                }}
+              />
+            </div>
+          ))}
+        </Slider>
+      </Container>
+    </div>
   );
 };
 
