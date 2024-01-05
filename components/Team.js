b40774031ec30a2a79f684dbc6f406d28b580890
komalahire <komala@navgurukul.org>
2024-01-05 16:07:22 +0530

    added some style

--- components/Team.js
+++ components/Team.js
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Typography, Container, Divider, Grid, Box,Button } from '@mui/material';
-import Image from 'next/image';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Typography,
+  Container,
+  Divider,
+  Grid,
+  Box,
+  Button,
+} from "@mui/material";
+import Image from "next/image";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { breakpoints } from "../styles/constant";
-
 
 const Team = () => {
   const isActive = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)");
@@ -19,7 +25,7 @@ const Team = () => {
     const fetchTeamData = async () => {
       try {
         const response = await axios.get(
-          'https://navgurukul.github.io/tarabai-shinde/data/meraki_team.json'
+          "https://navgurukul.github.io/tarabai-shinde/data/meraki_team.json"
         );
         const team = response.data;
         const supporters = [];
@@ -28,13 +34,13 @@ const Team = () => {
         Object.values(team).forEach((item) => {
           if (
             item.SamyarthTeam !== null &&
-            item.Association !== 'Not Active' &&
+            item.Association !== "Not Active" &&
             item.Association !== null &&
             item.Photo !== null &&
             item.Name !== null &&
             item.Designation !== null
           ) {
-            if (item.Association === 'Volunteer') {
+            if (item.Association === "Volunteer") {
               supporters.push(item);
             } else {
               teamMember.push(item);
@@ -45,12 +51,12 @@ const Team = () => {
 
         setTeamData({ supporters, teamMember: shuffledTeamMember });
       } catch (error) {
-        console.error('Error fetching team data:', error);
+        console.error("Error fetching team data:", error);
       }
     };
 
     fetchTeamData();
-  }, []); 
+  }, []);
 
   const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -70,19 +76,19 @@ const Team = () => {
         mt: "48px",
       }}
     >
-    <Typography variant="h4" textAlign={"center"} mb={"32px"}>
-      The Team
-      <Divider
-        variant="middle"
-        sx={{
-          background: "#4A9088",
-          height: "3px",
-          width: "40%",
-          margin: "auto",
-        }}
-      />
-    </Typography>
-    {isActive ? (
+      <Typography variant="h4" textAlign={"center"} mb={"32px"}>
+        The Team
+        <Divider
+          variant="middle"
+          sx={{
+            background: "#4A9088",
+            height: "3px",
+            width: "40%",
+            margin: "auto",
+          }}
+        />
+      </Typography>
+      {isActive ? (
         <Container
           sx={{
             display: "flex",
@@ -90,7 +96,6 @@ const Team = () => {
             WebkitOverflowScrolling: "touch",
           }}
         >
-      
           {teamData.teamMember.map((teamMember, index) => (
             <div
               key={index}
@@ -98,38 +103,67 @@ const Team = () => {
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
-                margin: "0 34px", 
+                margin: "0 34px",
               }}
             >
-              <Image src={teamMember.Photo.replace(/^http:\/\//i, 'https://')} alt={teamMember.Name} width={100} height={100} style={{ borderRadius: '50%' }} />
-              <Typography variant="Subtitle1"  mt={2} textAlign="center" >{teamMember.Name}</Typography>
-              <Typography variant="body1" color={'#6D6D6D'}  mt={2} textAlign="center">{teamMember.Designation}</Typography>
-           </div>
+              <Image
+                src={teamMember.Photo.replace(/^http:\/\//i, "https://")}
+                alt={teamMember.Name}
+                width={100}
+                height={100}
+                style={{ borderRadius: "50%" }}
+              />
+              <Typography variant="Subtitle1" mt={2} textAlign="center">
+                {teamMember.Name}
+              </Typography>
+              <Typography
+                variant="body1"
+                color={"#6D6D6D"}
+                mt={2}
+                textAlign="center"
+              >
+                {teamMember.Designation}
+              </Typography>
+            </div>
           ))}
-      
-        </Container>): (
-          <Grid container spacing={2} justifyContent="center" alignItems="center">
-            {teamData.teamMember.slice(0, showMore ? teamData.teamMember.length : 4).map((teamMember, index) => (
+        </Container>
+      ) : (
+        <Grid container spacing={2} justifyContent="center" alignItems="center">
+          {teamData.teamMember
+            .slice(0, showMore ? teamData.teamMember.length : 4)
+            .map((teamMember, index) => (
               <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
                 <Box textAlign="center">
-                  <Image src={teamMember.Photo.replace(/^http:\/\//i, 'https://')} alt={teamMember.Name} width={100} height={100} style={{ borderRadius: '50%' }} />
+                  <Image
+                    src={teamMember.Photo.replace(/^http:\/\//i, "https://")}
+                    alt={teamMember.Name}
+                    width={100}
+                    height={100}
+                    style={{ borderRadius: "50%" }}
+                  />
                 </Box>
                 <Box textAlign="center">
                   <Typography variant="body1">{teamMember.Name}</Typography>
-                  <Typography variant="body1" color={'#6D6D6D'}>{teamMember.Designation}</Typography>
+                  <Typography variant="body1" color={"#6D6D6D"}>
+                    {teamMember.Designation}
+                  </Typography>
                 </Box>
               </Grid>
             ))}
-            {!showMore && teamData.teamMember.length > 4 && (
-              <Grid item xs={12} sx={{ mt: 2, textAlign: 'center' }}>
-                <Typography variant="body1"  color={ "#4A9088"} style={{ cursor: 'pointer' }} onClick={handleSeeMoreClick}>
-                  See More Teams
-                </Typography>
-              </Grid>
-            )}
-          </Grid>
-        
-        )}
+          {!showMore && teamData.teamMember.length > 4 && (
+            <Grid item xs={12} sx={{ mt: 3, textAlign: "center" }}>
+              <Typography
+                variant="body1"
+                color={"#4A9088"}
+                style={{ cursor: "pointer", fontWeight: "bold" }}
+                onClick={handleSeeMoreClick}
+              >
+                See More Teams...
+              </Typography>
+            </Grid>
+          )}
+        </Grid>
+      )}
     </Container>
   );
 };
